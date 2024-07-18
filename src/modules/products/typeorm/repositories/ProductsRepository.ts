27dcb65457c0c941e0179374e36d62f6f0ba6358
Nathan <nathan.martinid@gmail.com>
2024-07-18 14:42:11 -0300

    creating ProductsRepository

---
+++ src/modules/products/typeorm/repositories/ProductsRepository.ts
@@ -0,0 +1,13 @@
+import { Products } from '../entities/product';
+import { AppDataSource } from '../data-source/data-source';
+
+export const UserRepository = AppDataSource.getRepository(Products).extend({
+  async findByName(name: string) {
+    const product = this.findOne({
+      where: {
+        name,
+      },
+    });
+    return product;
+  },
+});
